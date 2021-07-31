a2dcc5485b77c7d126ad8fee1959b0451711f11c
湛斯南 <zsnpromise@163.com>
2021-07-31 17:46:27 +0800

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c264ae6548abb8ccfb1ca9b681ed212d'),
+    },
+  },
+});
