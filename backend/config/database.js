89ef1387c286d8b126ce070c85b0432c71caade5
湛斯南 <zsnpromise@163.com>
2021-08-01 22:05:41 +0800

    a

--- backend/config/database.js
+++ backend/config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
-        username: env('DATABASE_USERNAME', 'anan'),
+        username: env('DATABASE_USERNAME', 'antttt'),
         password: env('DATABASE_PASSWORD', 'anan147258'),
       },
       options: {},
